b5980323f1251b3fa61d4007a7a23975d50339de
Nikhil Dev <nikhildev203@gmail.com>
2019-03-19 16:10:58 +0530

    Testing

---
+++ Seleu/src/main/java/TestingAuto.java
@@ -0,0 +1,23 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingAuto {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8081/customerReg/CustomerDetails.html");
+		 
+		Thread.sleep(3000);
+		 
+		driver.findElement(By.name("t1")).sendKeys("Nikhil Dev");
+		driver.findElement(By.name("t2")).sendKeys("nikhildev203@gmail.com");
+		driver.findElement(By.name("t3")).sendKeys("9742107503");
+		 
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		 
+		}
+}
